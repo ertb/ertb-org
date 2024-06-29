1a267a12295c803f46bac403269642dd72dd2951
Phil Batey <pbatey@gmail.com>
2024-06-29 03:59:12 -0600

    fix: a11y avoid opening new window

--- client/src/pages/home/download-link.tsx
+++ client/src/pages/home/download-link.tsx
@@ -11,7 +11,7 @@ export const DownloadLink = ({url, iconOnly}:Props) => {
   const filename = decodeURIComponent(n[n.length-1])
 
   return (
-    <a href={url} download target="_blank"
+    <a href={url} download
       className={cn(iconOnly ? "w-10 shrink-0 justify-center items-center" : "text-sm text-left justify-between w-full px-3 py-2", "flex rounded-md border border-input bg-background ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50")}
     >{iconOnly ? undefined : <span>{filename}</span>}
     <DownloadIcon className={cn(!iconOnly && "float-right")}/>
