df2dd6d656a588831beb087abb5a8d9fde0b35ce
Phil Batey <pbatey@gmail.com>
2024-06-29 04:03:40 -0600

    fix: a11y contrast ration in footer

--- client/src/components/app-footer.tsx
+++ client/src/components/app-footer.tsx
@@ -1,24 +1,13 @@
 import { useClientConfig } from "@/contexts/client-config-context"
-import { BookmarkIcon } from "@radix-ui/react-icons"
-import { useToast } from "./ui/use-toast"
 
 export const AppFooter = () => {
   const {version, commit} = useClientConfig()
-  const {toast} = useToast()
-
-  const showVersion = () => (version||commit) && toast({
-    title: `Version: ${version} ${commit.slice(0,7)}`.trim(),
-    className: 'dark'
-})
 
   return (
     <footer className="py-2 flex justify-between px-4 mt-auto text-white bg-slate-900 text-sm">
       <span></span>
       <span className='text-center'>Copyright © Electronic Recording Technology Board 2017-2024</span>
-      <button className='text-right text-slate-600' onClick={showVersion}>
-        <span className="sr-only">Show Version</span>
-        <BookmarkIcon/>
-      </button>
+      <span className='text-right'>{version}{commit?`(${commit})`:''}</span>
     </footer>
   )
 }
